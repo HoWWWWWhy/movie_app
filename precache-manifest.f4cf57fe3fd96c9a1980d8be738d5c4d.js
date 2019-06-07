c093cee0eb5bc69bea717d0030ebc84ec674ec40
BYUNSEHEE <howwwwwhy@gmail.com>
2019-06-07 13:04:59 +0900

    Updates

---
+++ precache-manifest.f4cf57fe3fd96c9a1980d8be738d5c4d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82d422819147c788885abc343e3390b4",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "20eb74b35a5e62f9bf45",
+    "url": "/movie_app/static/css/main.88186e8a.chunk.css"
+  },
+  {
+    "revision": "5cdb4062c6dfdd077298",
+    "url": "/movie_app/static/js/2.31ebf22a.chunk.js"
+  },
+  {
+    "revision": "20eb74b35a5e62f9bf45",
+    "url": "/movie_app/static/js/main.97e918b5.chunk.js"
+  },
+  {
+    "revision": "c6514544d436775abdc2",
+    "url": "/movie_app/static/js/runtime~main.175dda75.js"
+  }
+]);
